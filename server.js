4f5fb84f970eb4089ceac3e464604892359db8f5
SSL <ssl@MacBook-Pro-15-di-Claudio-218.local>
2026-03-31 10:02:51 +0200

    fix port railway

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.post("/generate-email", (req, res) => {
 });
 
 // START SERVER
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
